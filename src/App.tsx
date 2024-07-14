b0639274d1db33e303e27d7af28b875896ed0dc1
andymrrr <42513348+andymrrr@users.noreply.github.com>
2024-07-14 09:19:22 -0400

    Proyecto

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  StatusBar,
+  View,
+} from 'react-native';
+
+
+
+
+function App(){
+ 
+
+  return (
+    <View >
+      <StatusBar
+        barStyle={ 'light-content'}
+        backgroundColor={"black"}
+      />
+
+    </View>
+  );
+}
+
+
+export default App;
