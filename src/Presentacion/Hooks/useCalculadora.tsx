92683ca4694932253645968438523a0784044f07
andymrrr <42513348+andymrrr@users.noreply.github.com>
2024-07-15 21:12:59 -0400

    Botones Operacion Aritmetica

--- src/Presentacion/Hooks/useCalculadora.tsx
+++ src/Presentacion/Hooks/useCalculadora.tsx
@@ -1,19 +1,110 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
+enum Operador {
+  suma,
+  resta,
+  multiplicar,
+  dividir
+}
 export const useCalculadora = () => {
 
     const [numero, setNumero] = useState("0")
+    const [numeroAnterior, setNumeroAnterior] = useState("0")
+    const operacionAnterior = useRef<Operador>();
 
-    const ConstruirNumero = (numeroString: string)=>{
-        if(numero.includes(".") && numeroString ===".")
-        {
-            return;
+    const ConstruirNumero = (numeroString: string) => {
+      if (numero.includes('.') && numeroString === '.') return;
+
+      if (numero.startsWith('0') || numero.startsWith('-0')) {
+        // Punto decimal
+        if (numeroString === '.') {
+          return setNumero(numero + numeroString);
         }
-        setNumero(numero + numeroString)
+
+        // Evaluar si es otro cero y no hay punto
+        if (numeroString === '0' && numero.includes('.')) {
+          return setNumero(numero + numeroString);
+        }
+
+        // Evaluar si es diferente de cero, no hay punto, y es el primer numero
+        if (numeroString !== '0' && !numero.includes('.')) {
+          return setNumero(numeroString);
+        }
+
+        // Evitar 000000.00
+        if (numeroString === '0' && !numero.includes('.')) {
+          return;
+        }
+
+        return setNumero(numero + numeroString);
+      }
+
+      setNumero(numero + numeroString);
+    };
+  const alternarSimbolo = () => {
+    if ( numero.includes( '-' ) ) {
+      return setNumero( numero.replace( '-', '' ) );
     }
+
+    setNumero( '-' + numero );
+  };
+  const limpiar = ()=>{
+    setNumero("0")
+    setNumeroAnterior("0")
+  }
+
+  const eliminarOperacion = ()=>{
+    let operacionActual = ''
+    let numeroTemporal = numero
+    if(numero.includes("-")){
+      operacionActual ='-'
+      numeroTemporal = numero.substring(1)
+    }
+    if(numeroTemporal.length > 1){
+      return setNumero(operacionActual + numeroTemporal.slice(0,-1))
+    }
+    setNumero("0")
+  }
+  const asignarNumeroAnterior = ()=> {
+    if (numero.endsWith(".")) {
+      setNumeroAnterior(numero.slice(0,-1))
+    } else {
+      setNumeroAnterior(numero)
+    }
+    setNumero("0")
+  }
+
+  const operacionDividir =()=>{
+    asignarNumeroAnterior()
+    operacionAnterior.current = Operador.dividir;
+
+  }
+  const operacionMultiplicar =()=>{
+    asignarNumeroAnterior()
+    operacionAnterior.current = Operador.multiplicar;
+    
+  }
+  const operacionSumar =()=>{
+    asignarNumeroAnterior()
+    operacionAnterior.current = Operador.suma;
+    
+  }
+  const operacionResta =()=>{
+    asignarNumeroAnterior()
+    operacionAnterior.current = Operador.resta;
+    
+  }
   return{
     numero,
+    numeroAnterior,
 
-    ConstruirNumero
+    ConstruirNumero,
+    alternarSimbolo,
+    limpiar,
+    eliminarOperacion,
+    operacionDividir,
+    operacionMultiplicar,
+    operacionSumar,
+    operacionResta
   }
 }
--- src/Presentacion/Pantallas/Calculadora-Pantalla.tsx
+++ src/Presentacion/Pantallas/Calculadora-Pantalla.tsx
@@ -5,55 +5,86 @@ import { Boton } from '../Componentes/Boton';
 import { useCalculadora } from '../Hooks/useCalculadora';
 
 export const CalculadoraPantalla = () => {
-    const {numero,ConstruirNumero} = useCalculadora()
+    const {numero,numeroAnterior, ConstruirNumero,alternarSimbolo,limpiar, eliminarOperacion, 
+           operacionDividir,operacionMultiplicar,operacionResta,operacionSumar} = useCalculadora()
   return (
     <View style={EstiloGlobales.CalculadoraContenedor}>
       <View style={{paddingHorizontal: 30, marginBottom: 20}}>
-        <Text style={EstiloGlobales.Resultado}>{numero}</Text>
-        <Text style={EstiloGlobales.SubResultado}>15</Text>
+        <Text
+          adjustsFontSizeToFit
+          numberOfLines={1}
+          style={EstiloGlobales.Resultado}>
+          {numero}
+        </Text>
+        <Text
+          adjustsFontSizeToFit
+          numberOfLines={1}
+          style={EstiloGlobales.SubResultado}>
+          {(numeroAnterior ==="0") ? " " : numeroAnterior}
+        </Text>
       </View>
       <View style={EstiloGlobales.Fila}>
         <Boton
-          onPress={() => {}}
+          onPress={() => limpiar()}
           titulo="C"
           color={Colores.GrisClaro}
           textoNegro
         />
         <Boton
-          onPress={() => {}}
+          onPress={() => alternarSimbolo()}
           titulo="+/-"
           color={Colores.GrisClaro}
           textoNegro
         />
         <Boton
-          onPress={() => {}}
+          onPress={() => eliminarOperacion()}
           titulo="del"
           color={Colores.GrisClaro}
           textoNegro
         />
-        <Boton onPress={() => {}} titulo="/" color={Colores.Naranja} />
+        <Boton
+          onPress={() => operacionDividir()}
+          titulo="/"
+          color={Colores.Naranja}
+        />
       </View>
       <View style={EstiloGlobales.Fila}>
-        <Boton onPress={() => ConstruirNumero("7")} titulo="7" />
-        <Boton onPress={() => ConstruirNumero("8")} titulo="8" />
-        <Boton onPress={() => ConstruirNumero("9")} titulo="9" />
-        <Boton onPress={() => {}} titulo="X" color={Colores.Naranja} />
+        <Boton onPress={() => ConstruirNumero('7')} titulo="7" />
+        <Boton onPress={() => ConstruirNumero('8')} titulo="8" />
+        <Boton onPress={() => ConstruirNumero('9')} titulo="9" />
+        <Boton
+          onPress={() => operacionMultiplicar()}
+          titulo="X"
+          color={Colores.Naranja}
+        />
       </View>
       <View style={EstiloGlobales.Fila}>
-        <Boton onPress={() => ConstruirNumero("4")} titulo="4" />
-        <Boton onPress={() => ConstruirNumero("5")} titulo="5" />
-        <Boton onPress={() => ConstruirNumero("6")} titulo="6" />
-        <Boton onPress={() => {}} titulo="-" color={Colores.Naranja} />
+        <Boton onPress={() => ConstruirNumero('4')} titulo="4" />
+        <Boton onPress={() => ConstruirNumero('5')} titulo="5" />
+        <Boton onPress={() => ConstruirNumero('6')} titulo="6" />
+        <Boton
+          onPress={() => operacionResta()}
+          titulo="-"
+          color={Colores.Naranja}
+        />
       </View>
       <View style={EstiloGlobales.Fila}>
-        <Boton onPress={() => ConstruirNumero("1")} titulo="1" />
-        <Boton onPress={() => ConstruirNumero("2")} titulo="2" />
-        <Boton onPress={() => ConstruirNumero("3")} titulo="3" />
-        <Boton onPress={() => {}} titulo="+" color={Colores.Naranja} />
+        <Boton onPress={() => ConstruirNumero('1')} titulo="1" />
+        <Boton onPress={() => ConstruirNumero('2')} titulo="2" />
+        <Boton onPress={() => ConstruirNumero('3')} titulo="3" />
+        <Boton
+          onPress={() => operacionSumar()}
+          titulo="+"
+          color={Colores.Naranja}
+        />
       </View>
       <View style={EstiloGlobales.Fila}>
-        <Boton onPress={() => ConstruirNumero("0")} titulo="0" tamanoDoble={true} />
-        <Boton onPress={() => ConstruirNumero(".")} titulo="." />
+        <Boton
+          onPress={() => ConstruirNumero('0')}
+          titulo="0"
+          tamanoDoble={true}
+        />
+        <Boton onPress={() => ConstruirNumero('.')} titulo="." />
         <Boton onPress={() => {}} titulo="=" color={Colores.Naranja} />
       </View>
     </View>
